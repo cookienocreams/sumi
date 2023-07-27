93831f44edc37cff2fbf0c765e125b8397c0b2f1
cookienocreams <58923195+cookienocreams@users.noreply.github.com>
2023-07-27 12:00:40 -0700

    Fixed quality score calc + set default cores to 4
    Two interesting changes. First, I have been calculating the average quality score incorrectly. I have now changed the functions and added documentation showing why change was needed. Second, I have lowered the default number of threads to 4. This was after noticing that bowtie2 alignment is much faster using 4 threads than a higher number like 12. I'm guess it has something to do with the cost-benefit between needing to coordinate data between threads and the increased efficacy of splitting processing. Not sure, will have to look into it more later.

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,8 @@ lazy_static! {
     static ref DUAL_INDEX_REGEX: Regex = Regex::new(r"1:N:\d:[ATCGN]+\+[ATCGN]+").unwrap();
     static ref READ_NAME_UMI_REGEX: Regex = Regex::new(r"_([ATCG]+)").unwrap();
     static ref UMI_REGEX_QIAGEN: Regex = Regex::new(r"AACTGTAGGCACCATCAAT([ATCG]{12})AGATCGGAAG").unwrap();
+    static ref NUM_CAPTURE_REGEX: Regex =  Regex::new(r"\.\{(\d+)\}").unwrap();
+    static ref BASE_CAPTURE_REGEX: Regex = Regex::new(r"[ATCG]+").unwrap();
 }
 
 /// The `Config` struct is used to store the configuration settings for the analysis.
@@ -192,46 +194,82 @@ pub fn is_gzipped(filename: &str) -> io::Result<bool> {
 }
 
 /// Calcualte the mean of an input vector
-pub fn mean(numbers: &Vec<f64>) -> f64 {
-    let sum: f64 = numbers.iter().sum();
-    sum as f64 / numbers.len() as f64
+pub fn mean(numbers: &Vec<f32>) -> f32 {
+    let sum: f32 = numbers.iter().sum();
+    sum as f32 / numbers.len() as f32
 }
 
-/// Calculate the average quality score of a FASTQ file.
+/// Get the total error probability from a record's quality scores
 ///
+/// This function accepts ASCII-encoded quality score and produces a scalar
+/// value that estimates the 'total error probability' for that record.
+/// It means that if you want to calculate average quality score, you don't just sum
+/// up all the phred scores and find the average, but rather consider the 
+/// probability distribution of the scores.
+///
+/// The Phred score for a base `Q` is calculated as `Q = ASCII value of quality score - 33`.
+/// The error probability `P` for that base is then calculated as `P = 10^(-Q/10)`.
+/// Then these probabilities are summed up for all the bases to derive total error.
+/// 
 /// # Arguments
 ///
-/// * `input_fastq` - A string slice that holds the name of the FASTQ file to read.
+/// `q_score` - The quality scores of the current record as an ASCII-encoded byte string.
 ///
 /// # Returns
 ///
-/// This function will return the average quality score of the input FASTQ file. If an
-/// error occurs while reading the file, an error will be returned instead.
+/// The total error probability for the record.
+///
+/// # References
+///
+/// - Illumina's explanation on quality scores and their corresponding error rates:
+///   https://www.illumina.com/science/technology/next-generation-sequencing/plan-experiments/quality-scores.html
+pub fn get_q_score_probability(q_score: &[u8]) -> f32 {
+    let mut probability_sum = 0.0;
+    for &char in q_score.iter() {
+        let phred = *&char as f32 - 33.0;
+        let prob = 10.0_f32.powf(-phred / 10.0);
+        probability_sum += prob
+    }
+    probability_sum
+}
+
+/// Calculate the average quality score of a FASTQ file
+///
+/// This function will calculate the average quality score for all records in a FASTQ file.
+/// The function will calculate total error probability for a record from it's quality score 
+/// using ` get_q_score_probability` function. This value represents the probability of error 
+/// for each base, then divided by total number of sequences in fastq file. The per-base error 
+/// rates are then transformed back to a Phred scale and averaged across all bases.
+///
+/// # Arguments
+///
+/// * `input_fastq` - Path of the FASTQ file to calculate average quality score.
+///
+/// # Returns
+///
+/// - The average Phred Quality Score of all bases in the FASTQ file
+///
+/// # Errors
+/// 
+/// This function will return an error if:
+/// - The FASTQ file cannot be opened for any reason.
+/// - There is an interruption while reading the file.
 ///
 /// # Examples
-///
 /// ```
 /// let avg_quality = average_read_quality("sample.fastq")?;
 /// println!("The average quality score is {}.", avg_quality);
 /// ```
-pub fn average_read_quality(input_fastq: &str) -> Result<f64, Box<dyn std::error::Error>> {
+pub fn average_read_quality(input_fastq: &str) -> Result<f32, Box<dyn std::error::Error>> {
     let file = File::open(input_fastq)?;
     let reader = Reader::new(MultiGzDecoder::new(BufReader::new(file)));
 
     let mut q_score_list = Vec::new();
 
     for record_result in reader.records() {
-        let record = record_result.expect("Error during fastq record parsing");
-        if record.is_empty() {
-            let _check = record.check();
-            break;
-        }
-    
-        let quals = record.qual();
-        let sum_qual: f64 = quals.iter().map(|&c| c as f64 - 33.0).sum();
-
-        let q_score = sum_qual / record.seq().len() as f64;
-
+        let record = record_result?;
+        let prob_q_score = get_q_score_probability(record.qual()) / record.seq().len() as f32;
+        let q_score = -10.0 * prob_q_score.log10();
         q_score_list.push(q_score);
     }
 
@@ -318,19 +356,17 @@ pub fn average_quality_scores(fastq_files: &Vec<String>) -> Result<(), Box<dyn s
 /// let minimum_length = calculate_lengths(input_regex, min_length, is_qiagen);
 /// ```
 fn calculate_minimum_length(input_regex: &str, minimum_length: u8, is_qiagen: bool) -> u8 {
-    let num_capture_regex = Regex::new(r"\.\{(\d+)\}").unwrap();
-    let base_capture_regex = Regex::new(r"[ATCG]+").unwrap();
     let mut umi_sum: u8 = 0;
 
     // Get length of UMI by combining the numbers in the capture groups in the regex pattern
-    for captures in num_capture_regex.captures_iter(input_regex) {
+    for captures in NUM_CAPTURE_REGEX.captures_iter(input_regex) {
         if let Ok(num) = captures[1].parse::<u8>() {
             umi_sum += num;
         }
     }
 
     // Get length of intermediate bases between UMI groups if present
-    for capture in base_capture_regex.find_iter(input_regex) {
+    for capture in BASE_CAPTURE_REGEX.find_iter(input_regex) {
         umi_sum += capture.as_str().len() as u8;
     }
 
@@ -416,7 +452,7 @@ fn main() -> io::Result<()> {
                 .help("The number of processors to use for alignment.")
                 .value_name("NUM_THREADS")
                 .value_parser(clap::value_parser!(u8))
-                .default_value("12"),
+                .default_value("4"),
         )
         .arg(
             Arg::with_name("keep_intermediate_files")
